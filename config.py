f91c6a676b7ac7698a879c62d5cae1b59591c9e7
arpita330 <arpitanaha721@gmail.com>
2026-02-27 14:41:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ RIGHT_TOP = (872, 476)
 RIGHT_CENTER = (872, 476)
 
 # Template image URL (your base image)
-TEMPLATE_IMAGE_URL = "https://raw.github.com/arpita330/word_print/blob/main/IMG_20260215_144053_492.jpg"
+TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/arpita330/word_print/main/IMG_20260215_144053_492.jpg"
 
 # Font settings
 FONT_SIZE = 40
