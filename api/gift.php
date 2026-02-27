2eb982c3b78d9dd36ee7b08af91e8fcd0850889a
arpita330 <arpitanaha721@gmail.com>
2026-02-27 16:29:59 +0530

    Update gift.php

--- api/gift.php
+++ api/gift.php
@@ -1,26 +1,22 @@
 <?php
-// Move your existing PHP code here
-function generateGiftImage() {
-    $code = $_GET['code'] ?? 'NO CODE';
-    
-    $bgImage = 'https://raw.githubusercontent.com/arpita330/word_print/main/IMG_20260215_144053_492.jpg';
-    $img = imagecreatefromjpeg($bgImage);
-    
-    if (!$img) {
-        $img = imagecreatetruecolor(800, 600);
-        $bg = imagecolorallocate($img, 30, 30, 30);
-        imagefill($img, 0, 0, $bg);
-    }
-    
-    $gold = imagecolorallocate($img, 255, 215, 0);
-    
-    $codeX = (imagesx($img) - (strlen($code) * 30)) / 2;
-    imagestring($img, 5, $codeX, imagesy($img)/2 - 20, $code, $gold);
-    
-    header('Content-Type: image/jpeg');
-    imagejpeg($img, null, 90);
-    imagedestroy($img);
+header('Content-Type: image/jpeg');
+
+$code = $_GET['code'] ?? 'NO CODE';
+
+$bgImage = 'https://raw.githubusercontent.com/arpita330/word_print/main/IMG_20260215_144053_492.jpg';
+$img = imagecreatefromjpeg($bgImage);
+
+if (!$img) {
+    $img = imagecreatetruecolor(800, 600);
+    $bg = imagecolorallocate($img, 30, 30, 30);
+    imagefill($img, 0, 0, $bg);
 }
 
-generateGiftImage();
+$gold = imagecolorallocate($img, 255, 215, 0);
+
+$codeX = (imagesx($img) - (strlen($code) * 30)) / 2;
+imagestring($img, 5, $codeX, imagesy($img) * 0.7, $code, $gold);
+
+imagejpeg($img, null, 90);
+imagedestroy($img);
 ?>
