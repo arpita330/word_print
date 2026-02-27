f5dadbdbc86290cf39a4d98fcafd05c5beaf098e
arpita330 <arpitanaha721@gmail.com>
2026-02-27 16:15:35 +0530

    Update image.php

--- image.php
+++ image.php
@@ -2,6 +2,9 @@
 // Get code from URL
 $code = isset($_GET['code']) ? $_GET['code'] : 'NO CODE';
 
+// Set content type
+header('Content-Type: image/jpeg');
+
 // Background image URL
 $bgImage = 'https://raw.githubusercontent.com/arpita330/word_print/main/IMG_20260215_144053_492.jpg';
 
@@ -9,7 +12,7 @@ $bgImage = 'https://raw.githubusercontent.com/arpita330/word_print/main/IMG_2026
 $img = imagecreatefromjpeg($bgImage);
 
 if (!$img) {
-    // Fallback if image fails to load
+    // Create fallback image
     $img = imagecreatetruecolor(800, 600);
     $bg = imagecolorallocate($img, 50, 50, 50);
     imagefill($img, 0, 0, $bg);
@@ -17,19 +20,20 @@ if (!$img) {
 
 // Add text to image
 $gold = imagecolorallocate($img, 255, 215, 0);
+$black = imagecolorallocate($img, 0, 0, 0);
 $font_size = 5; // Built-in font size
 
 // Calculate position to center text
 $text_width = strlen($code) * imagefontwidth($font_size) * 2;
-$text_height = imagefontheight($font_size) * 2;
 $x = (imagesx($img) - $text_width) / 2;
 $y = (imagesy($img) / 2) + 20;
 
-// Draw the gift code
+// Add shadow for better visibility
+imagestring($img, $font_size, $x+2, $y+2, $code, $black);
+// Add main text
 imagestring($img, $font_size, $x, $y, $code, $gold);
 
 // Output as JPEG
-header('Content-Type: image/jpeg');
 imagejpeg($img, null, 90);
 imagedestroy($img);
 ?>
